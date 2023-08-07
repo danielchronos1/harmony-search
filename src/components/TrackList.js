8ed3bafc26c41f02ff4859930834c1db9caca62c
unknown <danielciburciu.1@gmail.com>
2023-08-07 17:09:02 +0300

    fix responsive

--- src/components/TrackList.js
+++ src/components/TrackList.js
@@ -3,8 +3,8 @@ import React from "react";
 const TrackList = ({ trackResults, handleTrackClick }) => {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-5xl font-bold mt-6 mb-6 tablet:text-xl">Search Results</h1>
-      <div className="grid grid-cols-3 grid-rows-3 gap-4 tablet:grid-cols-1">
+      <h1 className="text-5xl font-bold mt-6 mb-6 tablet:text-xl small:text-xl">Search Results</h1>
+      <div className="grid grid-cols-3 grid-rows-3 gap-4 tablet:grid-cols-1 small:grid-cols-1">
         {trackResults.map((trackItem) => (
           <div key={trackItem.track.track_id} className="flex flex-col border-2 p-6 text-center" onClick={() => handleTrackClick(trackItem)}>
             <h1 className="mb-5 font-bold">{trackItem.track.track_name}</h1>
